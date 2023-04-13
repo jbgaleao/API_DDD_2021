a71121a9f5407dc30f2d54c46e62d5865a0ddd12
Joao Galeao <jbgaleao@gmail.com>
2023-04-12 22:02:55 -0300

    Adicionar arquivos de projeto.

---
+++ Entidades/Notificacoes/Notifica.cs
@@ -0,0 +1,36 @@
+﻿ using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entidades.Notificacoes
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+            Notificacoes = new List<Notifica>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> Notificacoes;
+
+        public bool ValidaPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+    }
+}
